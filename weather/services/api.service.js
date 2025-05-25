04965cd613e0bd0c37458209bd1846f4249e94bd
Vladimir Noutbook <vovirus2@inbox.ru>
2025-05-25 12:33:19 +0300

    axios getwetaher

--- weather/services/api.service.js
+++ weather/services/api.service.js
@@ -1,5 +1,6 @@
-import https from 'https';
+// import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+import axios from 'axios';
 
 const getWeather = async (city) => {
     // const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
@@ -7,20 +8,32 @@ const getWeather = async (city) => {
     if (!token) {
         throw new Error('Отсутсвует токен, задайте его через -t');
     }
-    const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-    url.searchParams.append('q', city);
-    url.searchParams.append('appid', token);
-    url.searchParams.append('lang', 'ru');
-    url.searchParams.append('units', 'metric');
-    https.get(url, (response) => {
-        let res = '';
-        response.on('data', (chunk) => {
-            res += chunk;
-        });
-        response.on('end', () => {
-            console.log(res);
-        });
-    });
+    const { data } = await axios.get(
+        'https://api.openweathermap.org/data/2.5/weather',
+        {
+            params: {
+                q: city,
+                appid: token,
+                lang: 'ru',
+                units: 'metric',
+            },
+        }
+    );
+    return data;
+    // const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+    // url.searchParams.append('q', city);
+    // url.searchParams.append('appid', token);
+    // url.searchParams.append('lang', 'ru');
+    // url.searchParams.append('units', 'metric');
+    // https.get(url, (response) => {
+    // let res = '';
+    // response.on('data', (chunk) => {
+    // res += chunk;
+    // });
+    // response.on('end', () => {
+    // console.log(res);
+    // });
+    // });
 };
 export { getWeather };
 //10 минута
